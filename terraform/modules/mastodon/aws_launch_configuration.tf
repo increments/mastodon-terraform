79272355183512dfbe47d179a240bcdf5a31251c
Tomoya Chiba <tomo.asleep@gmail.com>
2017-09-01 20:11:20 +0900

    Use base name property value for LC's name prefix

--- terraform/modules/mastodon/aws_launch_configuration.tf
+++ terraform/modules/mastodon/aws_launch_configuration.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "mastodon" {
   iam_instance_profile        = "${aws_iam_instance_profile.mastodon.id}"
   image_id                    = "${lookup(var.aws_ecs_optimized_ami_ids, data.aws_region.current.name)}"
   instance_type               = "${var.aws_launch_configuration_mastodon_instance_type}"
-  name_prefix                 = "mastodon-"
+  name_prefix                 = "${var.aws_resource_base_name}-"
   security_groups             = ["${aws_security_group.mastodon_web.id}"]
   user_data                   = "${data.template_file.mastodon_aws_launch_configuration_user_data.rendered}"
 
